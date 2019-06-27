5275149c3230854f8484fddabca7fbdd7f58c96c
Eugene Antonov <github@antonovs.org>
2019-06-27 18:48:08 -0400

    added/updated license info

--- EA17.ClassLibrary/Generics/KiVi.cs
+++ EA17.ClassLibrary/Generics/KiVi.cs
@@ -1,4 +1,21 @@
-﻿using System;
+﻿//  # EA17.ClassLibrary
+//  C# .Net Core general class library 
+//
+//  Copyright(C) 2018-2019 Eugene Antonov
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of version 3 of the GNU General Public License
+//  as published by the Free Software Foundation.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.If not, see<https://www.gnu.org/licenses/>.
+
+using System;
 using System.Collections.Generic;
 
 namespace EA17.ClassLibrary.Generics
--- EA17.ClassLibrary/Storage/StorableObject.cs
+++ EA17.ClassLibrary/Storage/StorableObject.cs
@@ -12,6 +12,23 @@ namespace EA17.ClassLibrary.Storage
         protected Dictionary<Tag, string> Tags { get; } = new Dictionary<Tag, string>();
         public FieldSelection FieldSelection { get; } = new FieldSelection();
         public FieldOrder FieldOrder { get; } = new FieldOrder();
+        //  # EA17.ClassLibrary
+        //  C# .Net Core general class library 
+        //
+        //  Copyright(C) 2018-2019 Eugene Antonov
+        //
+        //  This program is free software: you can redistribute it and/or modify
+        //  it under the terms of version 3 of the GNU General Public License
+        //  as published by the Free Software Foundation.
+        //
+        //  This program is distributed in the hope that it will be useful,
+        //  but WITHOUT ANY WARRANTY; without even the implied warranty of
+        //  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+        //  GNU General Public License for more details.
+        //
+        //  You should have received a copy of the GNU General Public License
+        //  along with this program.If not, see<https://www.gnu.org/licenses/>.
+
 
         protected StorableObject() { Properties = ObjectProperties<StorableObject, Storable>.Get(GetType()); Init(); }
 
